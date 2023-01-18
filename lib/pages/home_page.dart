aa017b56aae7a3310fe5dfec944befe2563d36c9
Faisal <fnmghaithi@gmail.com>
2023-01-18 18:58:01 +0400

    Finished the forecast card

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,6 +13,11 @@ class _HomePageState extends State<HomePage> {
   bool isLoading = true;
   Weather? weather;
   WeatherService weatherService = WeatherService();
+  int currentHour = 0;
+  var hour;
+  var conditionIcon;
+  var conditionText;
+  var temp;
 
   @override
   void initState() {
@@ -22,6 +27,7 @@ class _HomePageState extends State<HomePage> {
 
   getWeatherData() async {
     weather = await weatherService.getWeather();
+    currentHour = int.parse(weather!.date.split(' ')[1].split(':')[0]);
     setState(() {
       isLoading = false;
     });
@@ -33,7 +39,9 @@ class _HomePageState extends State<HomePage> {
       backgroundColor: const Color.fromARGB(255, 122, 152, 220),
       body: isLoading
           ? const Center(
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
             )
           : Center(
               child: SafeArea(
@@ -66,61 +74,117 @@ class _HomePageState extends State<HomePage> {
                       ),
                       const SizedBox(height: 25.0),
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                '${weather!.temp}°',
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 7 * 9,
-                                  fontWeight: FontWeight.w100,
+                          Center(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  weather!.city,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 7 * 4,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              Text(
-                                weather!.city,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 7 * 4,
-                                  fontWeight: FontWeight.bold,
+                                Text(
+                                  '${weather!.temp}°',
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 7 * 9,
+                                    fontWeight: FontWeight.w100,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 3),
-                              Text(
-                                weather!.state,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 7 * 3,
-                                  fontWeight: FontWeight.w200,
+                                const SizedBox(height: 3),
+                                Text(
+                                  weather!.conditionText,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 7 * 4,
+                                    fontWeight: FontWeight.w400,
+                                  ),
                                 ),
-                              )
-                            ],
-                          ),
-                          const Spacer(),
-                          Column(
-                            children: <Widget>[
-                              Container(
-                                // color: Colors.red,
-                                width: 140,
-                                height: 110,
-                                child: Image.network(
-                                  'http://cdn.weatherapi.com/weather/64x64/day/113.png',
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                weather!.conditionText,
-                                style: const TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 7 * 3,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ],
                       ),
+                      const SizedBox(
+                        height: 25.0,
+                      ),
+                      SizedBox(
+                        height: 180,
+                        child: ListView.builder(
+                          physics: const BouncingScrollPhysics(),
+                          scrollDirection: Axis.horizontal,
+                          itemCount: weather!.forecast.length - 6 - 1,
+                          itemBuilder: (context, index) {
+                            hour = weather!.forecast[currentHour + index]
+                                    ['time']
+                                .split(' ')[1];
+                            conditionIcon = weather!
+                                .forecast[currentHour + index]['condition']
+                                    ['icon']
+                                .toString()
+                                .replaceAll('//', 'http://');
+                            conditionText =
+                                weather!.forecast[currentHour + index]
+                                    ['condition']['text'];
+                            temp = weather!.forecast[currentHour + index]
+                                ['temp_c'];
+                            print(hour);
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 5, horizontal: 7),
+                              child: Container(
+                                width: 120,
+                                decoration: BoxDecoration(
+                                  color:
+                                      const Color.fromARGB(18, 255, 255, 255),
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      hour,
+                                      style: const TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    SizedBox(
+                                      width: 60,
+                                      height: 60,
+                                      child: Image.network(
+                                        conditionIcon,
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                    Text(
+                                      temp.toString(),
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      conditionText,
+                                      style: const TextStyle(
+                                        color:
+                                            Color.fromARGB(139, 255, 255, 255),
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      )
                     ],
                   ),
                 ),
